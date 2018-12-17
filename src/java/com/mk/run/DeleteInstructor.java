fd8e7a5d5fa24ef1bdbecaa1b5a38642f9fae0d9
Mark Xu <isafesoft@hotmail.com>
2018-12-17 11:06:09 -0500

    2 one-2-one cascade delete instructor

--- src/java/com/mk/run/DeleteInstructor.java
+++ src/java/com/mk/run/DeleteInstructor.java
@@ -1,14 +1,12 @@
 package com.mk.run;
 
-import com.mk.hibernate.demo.Student;
 import com.mk.instructor.Instructor;
 import com.mk.instructor.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
-public class CreateInstructor {
+public class DeleteInstructor {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration().
                 configure().
@@ -18,32 +16,24 @@ public class CreateInstructor {
 
         // create session
         Session session = factory.getCurrentSession();
-
         try {
             // create the objects
-            Instructor tempInstructor = new Instructor("Chad", "Darby",
-                    "darby@gmail.com");
-
-            InstructorDetail tempInstructorDetail =
-                    new InstructorDetail("http://ytb.com/darby", "love code");
-
-            // associate the object
-            tempInstructor.setInstructorDetail(tempInstructorDetail);
-
-            System.out.println("Saving instructor:" + tempInstructor);
-            session.beginTransaction();
-            session.save(tempInstructor);
+            int id = 1;
+            Instructor tempInstructor =
+                    session.get(Instructor.class, id);
+            if(null != tempInstructor) {
+                // Note: will ALSO delete associated "details" object
+                // coz of CascadeType.ALL
+                session.delete(tempInstructor);
+            }
             session.getTransaction().commit();
             //
             // Note: this will ALSO save the details object
             // because of CascadeType.ALL
             //
-            session.beginTransaction();
 
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
     }
 }
